00a85bdc19ed578da3414c5e512284a87d02c95b
James Healy <jamesjeffershealy@gmail.com>
2013-11-15 17:44:55 -0500

    Created Achievements model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131115224233) do
+
+  create_table "achievements", force: true do |t|
+    t.string   "username"
+    t.string   "pic_url"
+    t.string   "title"
+    t.string   "message"
+    t.integer  "points"
+    t.integer  "time_ago"
+    t.string   "profile_url"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
