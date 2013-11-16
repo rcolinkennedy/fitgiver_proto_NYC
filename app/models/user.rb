4c85f700fa38e1d98fdd1c82f62a154997faf53e
R. Colin Kennedy <c@rcolinkennedy.com>
2013-11-15 19:19:38 -0500

    Twitter auth almost complete, working on final redirect

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  def apply_omniauth(omni)
+    authentications.build(:provider => omni['provider'], 
+                          :uid => omni['uid'], 
+                          :token => omni['credentials'].token, 
+                          :token_secret => omni['credentials'].secret)
+  end
+
+  def password_required?
+    (authentications.empty? || !password.blank?) && super #&& provider.blank?
+  end
+  
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+  
 end
